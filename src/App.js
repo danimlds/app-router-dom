99d7348bf1065285e772e2f4ac0b3ece6efe9c58
danimlds <daniel.mlds@gmail.com>
2023-01-03 20:28:21 -0300

    Carregamento dinâmico, nested routes e pagina 404

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 
 // config react router
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//components
+import Navbar from './components/Navbar';
+
+//pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Product from './pages/Product.js'
+import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
-import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -16,6 +23,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='products/:id' element={<Product />}/>
+          <Route path='products/:id/info' element={<Info />}/>
+          <Route path='*' element={<NotFound />}/>
+
         </Routes>
       </BrowserRouter>
     </div>
