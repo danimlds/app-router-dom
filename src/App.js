a3bfdfb855be3d8c6c6a6e2ad01dc5e7e766f14b
danimlds <daniel.mlds@gmail.com>
2023-01-04 20:06:03 -0300

    search params e redirecionamento de url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 // config react router
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 //components
 import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
 
 //pages
 import Home from './pages/Home';
@@ -12,6 +13,8 @@ import About from './pages/About';
 import Product from './pages/Product.js'
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+
 
 
 function App() {
@@ -20,13 +23,15 @@ function App() {
       <h1>REACT ROUTER</h1>
       <BrowserRouter>
       <Navbar />
+       <SearchForm />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='products/:id' element={<Product />}/>
           <Route path='products/:id/info' element={<Info />}/>
+          <Route path='/search' element={<Search />} />
+          <Route path='/company' element={<Navigate to="/about" />}/>
           <Route path='*' element={<NotFound />}/>
-
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./Navbar.css"
 
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre</Link>
+        {/*<Link to="/">Home</Link>
+        <Link to="/about">Sobre</Link>*/}
+        <NavLink to="/" /*className={({isActive}) => (isActive ? 'esta-ativo' : 'nao-ativo')}*/>Home</NavLink>
+        <NavLink to="/about" /*</nav> className={({isActive}) => (isActive ? 'esta-ativo' : 'nao-ativo')}*/>Sobre</NavLink>
     </nav>
   )
 }
